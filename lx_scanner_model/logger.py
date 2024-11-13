5f73ee2a37ea6d863b8efc96c61d0f678af49620
izofat <gorkemkacar12@gmail.com>
2024-11-14 00:27:34 +0300

    fix: logger

--- lx_scanner_model/logger.py
+++ lx_scanner_model/logger.py
@@ -4,6 +4,15 @@ import traceback
 
 class Logger:
     logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter("%(asctime)s - %(message)s")
+    ch.setFormatter(formatter)
+
+    logger.addHandler(ch)
 
     @classmethod
     def info(cls, message, *args):
